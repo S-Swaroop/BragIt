662fe832d920f248dde13515a33364e3427ddd9b
S-Swaroop <itsmesharan2000@gmail.com>
2021-06-24 22:52:31 +0530

    made changes in routes

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,16 +1,22 @@
 import {React , useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link , useHistory } from 'react-router-dom'
 
 import './Form.css'
 
 
 function Form(props) {
+    const history = useHistory();
     const [title, setTitle] = useState(props.title || "")
     const [text, setText] = useState(props.text || "" )
     const [email, setEmail] = useState(props.email || "" )
 
     const sumbitChanges = async(e)=>{
-        
+        if(text && title && email){
+            history.push('/')
+        }
+        else{
+            alert('The generated password has been sent to the provided email id')
+        }
     }
 
     return (
@@ -18,7 +24,7 @@ function Form(props) {
             <input type="text" className="input title" value={title} onChange={(e)=>setTitle(e.target.value)} />
             <input type="text" className="input text" value={text} onChange={(e)=>setText(e.target.value)} />
             <input type="text" className="input email" value={email} onChange={(e)=>setEmail(e.target.value)} />
-            <Link to={'/'}><input type="submit" className="submit-button" onClick={(e)=>sumbitChanges(e)} /></Link>
+            <input type="submit" className="submit-button" onClick={(e)=>sumbitChanges(e)} />
         </form>
     )
 }
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import {React, useState, useEffect } from 'react'
+import axios from 'axios'
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 function Home() {
+    const [blogs , setBlogs] = useState([{
+       'id' : 1,
+        'title' : "test",
+        'text': "testaga;j;"
+    }]);
+    
+
+
+    // useEffect(()=>{
+    //     const getData = async()=>{
+    //         const data = await axios.get(`${uri}/`).then(res=>res.data)
+    //         setBlogs([...data]);
+    //     }
+    //     getData() ;
+    // }, [])
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div >
+            <ul className="blog-list">
+                {blogs.map(blog => <div className="card">
+                    <div className="card-title" ><h1>{blog.title}</h1></div>
+                    <div className="card-body" ><h1>{blog.text}</h1></div>
+                    <Link to={`blogs/?id=${blog.id}`}><p>read more</p></Link>
+                </div>)}
+            </ul>
         </div>
     )
 }
