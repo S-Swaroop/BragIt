dcf5f175df95c32e8bfe1aaf47bf7000e97ef779
S-Swaroop <itsmesharan2000@gmail.com>
2021-06-26 17:02:37 +0530

    fixed search issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import { useState } from 'react'
 
 import Home from './components/Home/Home'
 import Post from './components/Post/Post'
@@ -8,23 +9,30 @@ import Edit from './components/Edit/Edit'
 import './App.css'
 
 function App() {
-
+  const [search, setSearch] = useState("")
   return (
     <Router>
         <div>
           <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <ul className="navbar-nav mr-auto">
               <li><Link to={'/'} className="nav-link"> Home </Link></li>
+              <li>
+                <form >
+                  <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)} />
+                  <Link to={`/?search=${search}`} ><input type="submit" value="search"/></Link>
+                </form>
+              </li>
             </ul>
             <ul className="navbar-nav ml-auto">
               <li><Link to={'/post'} className="nav-link"> Post </Link></li>
             </ul>
           </nav>
+          
           <Switch>
-              <Route exact path='/' component={Home} />
               <Route exact path='/post' component={Post}/>
               <Route path='/edit' component={Edit} />
               <Route path='/blogs' component={Details} />
+              <Route path='/' component={Home} />
           </Switch>
         </div>
       </Router>
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,29 +2,67 @@
 import {React, useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom';
+import queryString from 'query-string'
+import ReactModal from 'react-modal'
 
 //CSS:
 import './Home.css'
 
 function Home({location}) {
     const uri = `https://bragdrf.pythonanywhere.com/` ;
-    
+    const currentUrl = location.search;
     //States:
     const [blogs , setBlogs] = useState([]);
+    const [modal, setModal] = useState(false);
 
+    //Modal component design :
+    ReactModal.setAppElement(document.getElementById('custom-modal'))
+    const customStyles = {
+        content: {
+          top: '30%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
     //Fetching Data
     useEffect(()=>
     {
-        const getData = async()=>{
-            const data = await axios.get(`${uri}/`).then(res=>res.data).catch(err=>console.log(err))
-            setBlogs([...data])
+        const {search} = queryString.parse(currentUrl)
+        if(search===undefined)
+        {
+            const getData = async()=>{
+                const data = await axios.get(`${uri}/`).then(res=>res.data).catch(err=>console.log(err))
+                setBlogs([...data])
+            }
+            
+            getData();
+        }else{
+            const getData = async(q)=>{
+                const data = await axios.get(`${uri}/search/${q}/`).then(res=>res.data).catch(err=>console.log(err))
+                if(data){
+                    setBlogs([...data])
+                }
+                else{
+                    setModal(true);
+                }
+                console.log(data)
+            }
+            getData(search)
         }
         
-        getData();
-    }, [blogs, uri])
+    }, [blogs, uri, currentUrl])
 
     return (
         <div >
+            <div id="custom-modal">
+            <ReactModal isOpen={modal} style={customStyles} >
+                <h2>No such Blogs</h2>
+                <Link to='/'><button onClick={(e)=>{setModal(false)}}>Ok</button></Link>
+            </ReactModal>
+            </div>
             <ul className="blog-list container mt-4">
                 {blogs.map(blog => <div className="card mb-3 ">
                     <div className="card-title" ><h1>{blog.title}</h1></div>
