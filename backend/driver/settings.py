56c72b4cfef09029f416bc3a64ce906ac7ac53e0
Sauparna Gupta <sauparna.gupta@gmail.com>
2021-06-27 20:18:34 +0530

    Update settings.py

--- backend/driver/settings.py
+++ backend/driver/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bragit212@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hackonfriends2.0'
+EMAIL_HOST_PASSWORD = ********* #Password for smtp
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
